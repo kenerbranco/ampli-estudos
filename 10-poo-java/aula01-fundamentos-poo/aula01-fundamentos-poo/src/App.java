b58bbe70df20ab29f80a45fdc9652161aa248cac
Kener de Oliveira Branco <115368851+kenerbranco@users.noreply.github.com>
2023-01-21 13:18:01 -0300

    iniciei projeto e testes da aula teórica

---
+++ 10-poo-java/aula01-fundamentos-poo/aula01-fundamentos-poo/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Carro carroUm = new Carro("Punto", "Preto", 30000);
+
+        System.out.println(carroUm.modelo);
+    }
+}
---
+++ 10-poo-java/aula01-fundamentos-poo/aula01-fundamentos-poo/src/Carro.java
@@ -0,0 +1,11 @@
+public class Carro {
+    String modelo;
+    String cor;
+    double preco;
+
+    Carro(String modelo, String cor, double preco) {
+        this.modelo = modelo;
+        this.cor = cor;
+        this.preco = preco;
+    }
+}
