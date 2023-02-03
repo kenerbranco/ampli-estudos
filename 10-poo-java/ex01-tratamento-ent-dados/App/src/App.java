86bc689c0ebfeb4cdc33c5dd1b280eb0a3f3c9c2
Kener de Oliveira Branco <115368851+kenerbranco@users.noreply.github.com>
2023-02-03 13:42:51 -0300

    finalizei ex01

---
+++ 10-poo-java/ex01-tratamento-ent-dados/App/src/App.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        int i;
+        int codigo = 0;
+
+        do { 
+
+            try {
+                Scanner entrada = new Scanner(System.in);
+                System.out.println("Informe o código do produto [0 - 200]: ");
+                codigo = entrada.nextInt();
+    
+                if((codigo < 0) || (codigo > 200)) {
+
+                    System.out.println("Valor inválido, digite um valor entre 0 - 200!");
+                    i = 0;
+
+                } else { i = 1; }
+
+            } catch(Exception error) {
+
+                i = 0;
+                System.out.println("Valor inválido, tente novamente.");
+                System.out.println("Erro apresentado: " + error);
+            }
+
+        } while(i == 0);
+
+        System.out.println("Valor inserido: " + codigo);
+
+    }
+}
