8c848d652af27d43d4bfbba25491cdce145f5b17
Kener de Oliveira Branco <115368851+kenerbranco@users.noreply.github.com>
2023-02-07 12:19:13 -0300

    finalizei ex02 - class get/post

--- 10-poo-java/ex02-totamotors-class-model/App/src/App.java
+++ 10-poo-java/ex02-totamotors-class-model/App/src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Veiculo carroUm = new Veiculo();
+        Veiculo carroDois = new Veiculo();
+
+        carroUm.postAno(2011);
+        carroUm.postModelo("ELX 1.4");
+        carroUm.postCor("Preto");
+        carroUm.postPreco(30500);
+
+        System.out.println(carroUm.getModelo());
+
+        carroDois.postPreco(60200);
+        System.out.println(carroDois.getPreco());
+
     }
 }
---
+++ 10-poo-java/ex02-totamotors-class-model/App/src/Veiculo.java
@@ -0,0 +1,74 @@
+public class Veiculo {
+    
+    private int ano;
+    private String modelo;
+    private String cor;
+    private double preco;
+
+    // Funções GET
+    public int getAno() {
+        return this.ano;
+    }
+
+    public String getModelo() {
+        return this.modelo;
+    }
+
+    public String getCor() {
+        return this.cor;
+    }
+
+    public double getPreco() {
+        return this.preco;
+    }
+
+    // Funções POST
+    public void postAno(int ano) {
+        
+        try {
+            this.ano = ano;
+            System.out.println("Ano cadastrado com sucesso!");
+
+        } catch(Exception error) {
+            System.out.println("Erro ao cadastrar ano.");
+            System.out.println(error);
+        }  
+    }
+
+    public void postModelo(String modelo) {
+        
+        try {
+            this.modelo = modelo;
+            System.out.println("Modelo cadastrado com sucesso!");
+
+        } catch(Exception error) {
+            System.out.println("Erro ao cadastrar modelo.");
+            System.out.println(error);
+        }  
+    }
+
+    public void postCor(String cor) {
+        
+        try {
+            this.cor = cor;
+            System.out.println("Cor cadastrado com sucesso!");
+
+        } catch(Exception error) {
+            System.out.println("Erro ao cadastrar cor.");
+            System.out.println(error);
+        }  
+    }
+
+    public void postPreco(double preco) {
+        
+        try {
+            this.preco = preco;
+            System.out.println("Preço cadastrado com sucesso!");
+
+        } catch(Exception error) {
+            System.out.println("Erro ao cadastrar preço.");
+            System.out.println(error);
+        }  
+    }
+
+}
