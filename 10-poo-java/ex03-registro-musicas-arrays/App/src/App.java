0847574b8d9a9368189fa994ad56199219656d7c
Kener de Oliveira Branco <115368851+kenerbranco@users.noreply.github.com>
2023-02-09 14:27:21 -0300

    finalizei etapa 02 do projeto

--- 10-poo-java/ex03-registro-musicas-arrays/App/src/App.java
+++ 10-poo-java/ex03-registro-musicas-arrays/App/src/App.java
@@ -6,30 +6,56 @@ public class App {
         System.out.println("*** Ex03 - Projeto Registro Músicas [Arrays] ***");
 
         // Configuração tamanho das Arrays Código e Execuções das Músicas
-        int tamArr = 5;
+        int tamArr = 2;
 
         // Declaração das Arrays
         int arrayCodigo[] = new int[tamArr];
         int arrayExecucoes[] = new int[tamArr];
+        String arrayNome[] = new String[tamArr];
+        String arrayGenero[] = new String[tamArr];
+        String arrayAutor[] = new String[tamArr];
 
-        try (Scanner entrada = new Scanner(System.in)) {
-            // Gravar dados das músicas
-            for(int i=0; i<tamArr; i++) {
-                System.out.printf("[%d] Digite o código da música: ", i+1);
-                arrayCodigo[i] = entrada.nextInt();
-                System.out.printf("[%d] Digite a quantidade de execuções desta música: ", i+1);
-                arrayExecucoes[i] = entrada.nextInt();
-            }
+        // Declaração entradas da class Scanner
+        Scanner entrada = new Scanner(System.in);
+        Scanner entradaString = new Scanner(System.in);
 
-            //Imprimir dados das músicas
-            for(int i=0; i<tamArr; i++) {
-                System.out.printf("--- Música %d ---\n", i+1);
-                System.out.printf("Código: %d\n", arrayCodigo[i]);
-                System.out.printf("Execuções: %d\n", arrayExecucoes[i]);
-                System.out.println("-------------------");
-            }
-        } catch(Exception event) {
-            System.out.println("Error! " + event);
+        // Gravação dados das músicas
+        for(int i=0; i<tamArr; i++) {
+            // Entrada código da música
+            System.out.printf("[%d] Código da música: ", i+1);
+            arrayCodigo[i] = entrada.nextInt();
+
+            // Entrada nome da música
+            System.out.printf("[%d] Nome da música: ", i+1);
+            arrayNome[i] = entradaString.nextLine().toUpperCase();
+            
+            // Entrada gênero da música
+            System.out.printf("[%d] Gênero da música: ", i+1);
+            String entradaGenero = entradaString.nextLine();
+            arrayGenero[i] = entradaGenero.substring(0,entradaGenero.indexOf(" "));
+
+            // Entrada autor da música
+            System.out.printf("[%d] Autor da música: ", i+1);
+            String entradaAutor = entradaString.nextLine();
+            arrayAutor[i] = entradaAutor.substring(0,1).toUpperCase().concat(entradaAutor.substring(1));
+
+            // Entrada qtd execições da música
+            System.out.printf("[%d] Quantidade de execuções desta música: ", i+1);
+            arrayExecucoes[i] = entrada.nextInt();
         }
+
+        // Imprimir dados das músicas
+        for(int i=0; i<tamArr; i++) {
+            System.out.printf("--- Música %d ---\n", i+1);
+            System.out.printf("Código: %d\n", arrayCodigo[i]);
+            System.out.printf("Nome da música: %s\n", arrayNome[i]);
+            System.out.printf("Gênero da música: %s\n", arrayGenero[i]);
+            System.out.printf("Autor da música: %s\n", arrayAutor[i]);
+            System.out.printf("Execuções: %d\n", arrayExecucoes[i]);
+            System.out.println("-------------------");
+        }
+
+        entrada.close();
+        entradaString.close();
     }
 }
